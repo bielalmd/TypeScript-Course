f3f07b8fd858511bd0f60cfd53dd200cb41c704c
bielalmd <austbiel@gmail.com>
2024-01-04 11:37:39 -0300

    feat: working with tuples

--- app.js
+++ app.js
@@ -5,12 +5,17 @@
 var person = {
     name: 'Gabriel',
     age: 30,
-    hobbies: ['Photography', 'Sports']
+    hobbies: ['Photography', 'Sports'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+// person.role[1] = 10;
 var favoritePhotos;
 favoritePhotos = ['Sky'];
+console.log(person);
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+    // console.log(hobby.map());
 }
--- app.ts
+++ app.ts
@@ -3,15 +3,27 @@
 //     age: number
 // } = {
 
-const person = {
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+} = {
     name: 'Gabriel',
     age: 30,
-    hobbies: ['Photography', 'Sports']
+    hobbies: ['Photography', 'Sports'],
+    role: [2, 'author']
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']
 
 let favoritePhotos: string[]
 favoritePhotos = ['Sky']
 
+console.log(person);
 console.log(person.name);
 
 for(const hobby of person.hobbies) {
