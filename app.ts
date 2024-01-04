3d34dc49486d5297bcc0d813d9976812624dad1f
bielalmd <austbiel@gmail.com>
2024-01-04 11:04:30 -0300

    feat: types objects

--- app.ts
+++ app.ts
@@ -1,18 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    // if(typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //     throw new Error('Incorrect Input')
-    // }
-    const result = n1 + n2
-    if(showResult){
-        console.log(phrase + result);
-    } else {
-        return result
-    }
-}
+// const person: {
+//     name: string,
+//     age: number
+// } = {
 
-const number1 = 5;
-const number2 = 5.5;
-const printResult = true;
-const resultPhrase = 'Result is: '
+const person = {
+    name: 'Gabriel',
+    age: 30
+};
 
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name);
