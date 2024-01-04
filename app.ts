fe7cebf959a43097b58ea560f7df0d4662e37833
bielalmd <austbiel@gmail.com>
2024-01-04 11:22:52 -0300

    feat: arrays

--- app.ts
+++ app.ts
@@ -5,7 +5,16 @@
 
 const person = {
     name: 'Gabriel',
-    age: 30
+    age: 30,
+    hobbies: ['Photography', 'Sports']
 };
 
+let favoritePhotos: string[]
+favoritePhotos = ['Sky']
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby);
+    // console.log(hobby.map());
+}
