341965a7623faf082fba3896f896a95a84876462
bielalmd <austbiel@gmail.com>
2024-01-04 10:11:19 -0300

    feat: working with numbers, string and booleans

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-function add(n1: number, n2: number ) {
-    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error('Incorrect Input')
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect Input')
+    // }
+    const result = n1 + n2
+    if(showResult){
+        console.log(phrase + result);
+    } else {
+        return result
     }
-    return n1 + n2
 }
 
 const number1 = 5;
 const number2 = 5.5;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2)
-console.log(result);
+add(number1, number2, printResult, resultPhrase)
