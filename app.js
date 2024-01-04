f52e17637674e1a3cc936639021dfcfa5d05de64
bielalmd <austbiel@gmail.com>
2024-01-04 09:58:31 -0300

    feat: ts types vs js types

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Typescript its work');
+function add(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect Input');
+    }
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 5.5;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('Typescript its work');
+function add(n1: number, n2: number ) {
+    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error('Incorrect Input')
+    }
+    return n1 + n2
+}
+
+const number1 = 5;
+const number2 = 5.5;
+
+const result = add(number1, number2)
+console.log(result);
