56c097079515486435287f6b110ea4a499e77839
bielalmd <austbiel@gmail.com>
2024-01-05 11:42:21 -0300

    feat: literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2; // case you pass the input like a string won't to be possible a assignable with name like 'max' and the logic convert to a NaN
     } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    return result
+
+    // if(resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 
-const combineAges = combine(32, 26);
+const combineAges = combine(32, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Gabriel ', 'Gabriela');
+const combineStringAges = combine('32', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Gabriel ', 'Gabriela', 'as-text');
 console.log(combineNames);
