04c5098c08524455df40080dedba9d02d7114210
bielalmd <austbiel@gmail.com>
2024-01-05 13:45:54 -0300

    feat: unknown type

--- app.ts
+++ app.ts
@@ -1,28 +1,9 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+let userInput: unknown;
+let userName: string
+
+userInput = 5
+userInput = "Pedro"
+if (typeof userInput === 'string') {
+    console.log(userName = userInput);
 }
 
-function printResult(num: number): void {
-    console.log(`Result: ${num}`);
-}
-
-function addAndHandle(n1: number, n2: number, callBack: (num: number) => void) {
-    const result = n1 + n2;
-    callBack(result)
-}
-
-printResult(add(10, 15))
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-// console.log(combineValues(5, 5));
-
-// let someValue: undefined;
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-})
