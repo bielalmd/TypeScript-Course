557f80261496d97f573653a95576c3860a980f33
bielalmd <austbiel@gmail.com>
2024-01-05 13:21:04 -0300

    feat: function Types & CallBacks

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: ".concat(num));
 }
+function addAndHandle(n1, n2, callBack) {
+    var result = n1 + n2;
+    callBack(result);
+}
 printResult(add(10, 15));
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
-console.log(combineValues(5, 5));
+// console.log(combineValues(5, 5));
 // let someValue: undefined;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
     console.log(`Result: ${num}`);
 }
 
+function addAndHandle(n1: number, n2: number, callBack: (num: number) => void) {
+    const result = n1 + n2;
+    callBack(result)
+}
+
 printResult(add(10, 15))
 
 let combineValues: (a: number, b: number) => number;
@@ -14,6 +19,10 @@ combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 
-console.log(combineValues(5, 5));
+// console.log(combineValues(5, 5));
 
 // let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+})
