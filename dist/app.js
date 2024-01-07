e328a3f3df2189b5d5e6ddc17a4757b6403a1737
bielalmd <austbiel@gmail.com>
2024-01-07 11:21:26 -0300

    feat: diferences between let, var and const

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
+const userName = 'Gabriel';
+let age = 26;
+age = 28;
+function add(a, b) {
+    let result;
+    result = a + b;
+    return result;
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Lets go maboy
+const userName = 'Gabriel';
+
+// userName = 'Pedro';
+
+let age = 26;
+
+age = 28;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+// console.log(result);
+
+// if(age > 20) {
+//     let isOld = true;
+// }
+
+// console.log(isOld);
+
+// On TypeScript var is a global and function variable, so the TS identify var same as JavaScript
+// So un TS we use variable calls 'let'
