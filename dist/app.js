55d5e6bad61e559f41fe1c781fd494fa4e9d6c57
bielalmd <austbiel@gmail.com>
2024-01-09 20:19:25 -0300

    feat: first class

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,9 @@
 "use strict";
-const hobbies = ['Photography', 'Play Games'];
-const activeHobbies = ['Trip'];
-activeHobbies.push(...hobbies);
-console.log(activeHobbies);
-const person = {
-    firstName: 'Gabriel',
-    age: 26
-};
-const copiedPerson = Object.assign(Object.assign({}, person), { hobbies: 'Trip' });
-const add = (...num) => {
-    return num.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const concatStrings = (...strings) => {
-    return strings.reduce((s1, s2) => {
-        return s1 + s2;
-    }, 'hi: ');
-};
-const addStringNames = concatStrings('Gabriel ', 'And ', 'Gabriela');
-const addedNumbers = add(5, 10, 15, 0);
-const [hobby1, hobby2, ...remainsHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-const { firstName: userName, age } = person;
-console.log(userName, age, person);
+class Departament {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Departament('Accounting');
+console.log(accounting);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,11 @@
-// const userName = 'Gabriel';
+class Departament {
+    name: string;
 
-// userName = 'Pedro';
-
-// let age = 26;
-
-// age = 28;
-
-// function add(a: number, b: number) {
-//     let result;
-//     result = a + b;
-//     return result;
-// }
-
-// console.log(result);
-
-// if(age > 20) {
-//     let isOld = true;
-// }
-
-// console.log(isOld);
-
-// const add1 = (a: number, b: number = 1)  =>  a + b;
-// // default value in argument the have to be in last in the list
-
-// const printOutput: (a:number | string) => void = output => console.log(output);
-
-// const button = document.querySelector('button');
-
-// if (button){
-//     button.addEventListener('click', event => console.log(event));
-// }
-
-// printOutput(add1(1));
-
-const hobbies = ['Photography', 'Play Games'];
-const activeHobbies = ['Trip'];
-
-activeHobbies.push(...hobbies);
-console.log(activeHobbies);
-
-const person = {
-    firstName: 'Gabriel',
-    age: 26
+    constructor(n: string) {
+        this.name = n;
+    }
 }
 
-const copiedPerson = {
-    ...person, 
-    hobbies: 'Trip'
-};
 
-// console.log(person);
-// console.log(copiedPerson);
-
-const add = (...num: number[]) => {
-    return num.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-
-const concatStrings = (...strings: [string, string, string ]) => {
-    return strings.reduce((s1, s2) => {
-        return s1 + s2;
-    }, 'hi: ')
-}
-
-const addStringNames = concatStrings('Gabriel ', 'And ', 'Gabriela');
-// console.log(addStringNames);
-
-const addedNumbers = add(5,10,15,0)
-// console.log(addedNumbers);
-
-const [hobby1, hobby2, ...remainsHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-
-const {firstName: userName, age} = person;
-
-console.log(userName, age, person);
-
-// On TypeScript var is a global and function variable, so the TS identify var same as JavaScript
-// So un TS we use variable calls 'let'
+const accounting = new Departament('Accounting');
+console.log(accounting);
