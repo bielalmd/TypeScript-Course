361f785ec27088f3c43a4df1f060e6bade6b2fd4
bielalmd <austbiel@gmail.com>
2024-01-10 20:45:51 -0300

    feat: singletons and private constructors

--- dist/app.js
+++ dist/app.js
@@ -44,6 +44,13 @@ class AccountingDepartment extends Department {
         this.reports = reports;
         this.lastReport = reports[0];
     }
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
+    }
     describe() {
         console.log(`Accounting Department - ID ${this.id}`);
     }
@@ -68,7 +75,9 @@ it.addEmployee('Pedro');
 it.printEmployeeInfo();
 it.describe();
 console.log(it);
-const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+console.log(accounting, accounting2);
 accounting.mostRecentReports = 'Now i can see this';
 accounting.addReports('Something its rong on the back end');
 accounting.addEmployee('Gabriel');
--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,7 @@ class ITDepartment extends Department {
 
 class AccountingDepartment extends Department {
     private lastReport: string;
+    private static instance: AccountingDepartment;
 
     get mostRecentReports() {
         if (this.lastReport) {
@@ -53,9 +54,17 @@ class AccountingDepartment extends Department {
         this.addReports(value)
     }
 
-    constructor(id: string, private reports: string[]) {
+    private constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
         this.lastReport = reports[0];
+    }
+
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
     }
 
     describe() {
@@ -94,7 +103,10 @@ it.describe();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+console.log(accounting, accounting2);
 accounting.mostRecentReports = 'Now i can see this';
 accounting.addReports('Something its rong on the back end');
 // console.log(accounting.mostRecentReports);
