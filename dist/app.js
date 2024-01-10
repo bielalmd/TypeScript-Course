98291a4f7ca3ec91d408ad6af04ec1fe6babc4c3
bielalmd <austbiel@gmail.com>
2024-01-09 21:02:41 -0300

    feat: constructor func anda the this keyword

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,12 @@ class Departament {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
 }
 const accounting = new Departament('Accounting');
-console.log(accounting);
+accounting.describe();
+const accountingCopy = { name: 's', describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,17 @@ class Departament {
     constructor(n: string) {
         this.name = n;
     }
+
+    describe(this: Departament) {
+        console.log(`Department: ${this.name}`);
+    }
 }
 
 
 const accounting = new Departament('Accounting');
-console.log(accounting);
+// console.log(accounting);
+
+accounting.describe();
+
+const accountingCopy = { name: 's', describe: accounting.describe}
+accountingCopy.describe()
