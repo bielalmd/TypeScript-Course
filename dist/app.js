ede125cc855ac99141253bde9e3745a670d572f3
bielalmd <austbiel@gmail.com>
2024-01-09 22:14:30 -0300

    feat: provate and public access modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,23 @@
 "use strict";
 class Departament {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log(`Department: ${this.name}`);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Departament('Accounting');
+accounting.addEmployee('Gabriel');
+accounting.addEmployee('Pedro');
+accounting.printEmployeeInfo();
 accounting.describe();
-const accountingCopy = { name: 's', describe: accounting.describe };
-accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Departament {
     name: string;
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n;
@@ -8,13 +9,27 @@ class Departament {
     describe(this: Departament) {
         console.log(`Department: ${this.name}`);
     }
+
+    addEmployee(employee: string){
+        this.employees.push(employee)
+    }
+
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 
 const accounting = new Departament('Accounting');
 // console.log(accounting);
 
+accounting.addEmployee('Gabriel');
+accounting.addEmployee('Pedro');
+// accounting.employees[2] = 'Gabriela'
+accounting.printEmployeeInfo()
+
 accounting.describe();
 
-const accountingCopy = { name: 's', describe: accounting.describe}
-accountingCopy.describe()
+// const accountingCopy = { name: 's', describe: accounting.describe}
+// accountingCopy.describe()
