c364004c67a457ad083fce9d244ec648f858b697
bielalmd <bielalmd@gmail.com>
2024-01-12 15:48:29 -0300

    feat: more on type guards

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,47 @@
 "use strict";
 const e1 = {
     name: 'Gabriel',
-    privilages: ['creat-server'],
+    privileges: ['creat-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+console.log(add('1', '2'));
+function printEmployeeInfo(emp) {
+    console.log(`Name: ${emp.name}`);
+    if ('privileges' in emp) {
+        console.log(`Privileges: ${emp.privileges}`);
+    }
+    if ('startDate' in emp) {
+        console.log(`Start Date: ${emp.startDate}`);
+    }
+}
+printEmployeeInfo({ name: 'Gabriel', startDate: new Date() });
+class Car {
+    drive() {
+        console.log(' Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log(' Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log(`Loading cargo... ${amount}`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
