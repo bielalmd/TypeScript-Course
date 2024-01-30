bff7fbbfc0e0d63b8ea435a3b127e9b7cf4ae459
bielalmd <bielalmd@gmail.com>
2024-01-29 21:03:14 -0300

    feat: another generic function

--- src/app.js
+++ src/app.js
@@ -14,3 +14,14 @@ function merge(objA, objB) {
 }
 var mergeObj = merge({ name: 'Gabriel' }, { age: 26 });
 console.log(mergeObj.age);
+function countAndPrint(element) {
+    var descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndPrint(['photograhpy', 'Playing football']));
--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergeObj = merge({name: 'Gabriel'}, {age: 26});
 console.log(mergeObj.age);
+
+interface Lengthy {
+    length: number
+}
+
+function countAndPrint<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element'
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.'
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndPrint(['photograhpy','Playing football']));
